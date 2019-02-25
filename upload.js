6ed4a6472fe2fbd3445b5f7d831d51c133274efa
Lee Booth <leehopebooth@hotmail.com>
2019-02-25 17:09:52 +0000

    Improved Code

--- upload.js
+++ upload.js
@@ -18,16 +18,18 @@ app.post('/', function (req, res){
         console.log('Folder Created!')
         fs.mkdirSync(dir);
     }
+
     form.on('fileBegin', function (name, file){
         file.path = __dirname +'/upload/'+ file.name;
     });
-
+ 
     form.on('file', function (name, file){
         console.log('Uploaded ' + file.name);
     });
     console.log(__dirname);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Files Uploaded</h1>');
+    res.write('<a href="/">Back</a>');
     return res.end();
 });
 
