7108f6ab62618fa879aa72479bd5352a2d8a42b2
Ben Leadbeater <40888832+nagatok1@users.noreply.github.com>
2019-03-05 12:02:02 +0000

    Update ConvertToTxt.js

--- server/ConvertToTxt.js
+++ server/ConvertToTxt.js
@@ -16,27 +16,73 @@ watch(folder, { recursive: true }, function (evt, name) {
   
 
         if (name.endsWith(".pdf")) {
-            pfdToTxt(name, txtfile);
+            pdfToTxt(name, txtfile);
+            fs.unlinkSync(name);
+        }
+
+        if (name.endsWith(".xlsx")) {
+            xlsxToTxt(name, txtfile);
+            fs.unlinkSync(name);
+        }
+
+        if (name.endsWith(".docx")) {
+            docxToText(name, txtfile);
             fs.unlinkSync(name);
         }
 
     }
 });
 
-function pfdToTxt(name, txtfile) {
+function docxToText(name,txtfile) {
+    const fs = require("fs");
+    var mammoth = require("mammoth");
+
+    mammoth.convertToHtml({ path: name })
+        .then(function (result) {
+            let html = result.value;
+            let messages = result.messages;
+            fs.writeFile(txtfile, html, function (err) {
+                if (err) throw err;
+                console.log("Docx Converted");
+            })
+        })
+        .done();
+}
+
+function pdfToTxt(name, txtfile) {
     const fs = require("fs");
     const pdf = require('pdf-parse');
 
     let dataBuffer = fs.readFileSync(name);
 
     pdf(dataBuffer).then(function (data) {
-
-        fs.writeFile(txtfile, data.text, function (err) {
-            if (err) throw err;
-            console.log("PDF Converted !");
-        })
+        createDocx(txtfile,data.text);
     });
 }
 
 
+function xlsxToTxt(name, txtfile) {
+    var XLSX = require('xlsx')
+    var workbook = XLSX.readFile(name);
+    var sheet_name_list = workbook.SheetNames;
+    var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+    let xlText = JSON.stringify(xlData);
+    createDocx(txtfile, xlText)
+}
 
+
+function createDocx(txtfile,text){
+    let docx = require('docx');
+
+    let doc = new docx.Document();
+
+    let paragraph = new docx.Paragraph();
+    paragraph.addRun(new docx.TextRun(text));
+    doc.addParagraph(paragraph);
+
+
+    let packer = new docx.Packer();
+    packer.toBuffer(doc).then((buffer) => {
+    fs.writeFileSync((txtfile.substring(0,txtfile.length-4)+".docx"), buffer)
+    })
+};
