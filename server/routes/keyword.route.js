89ed8324c67f87b0a7335cc18c9d6724d6eafd42
LexusXBox <leehopebooth@hotmail.com>
2019-03-07 10:24:21 +0000

    Update keyword.route.js

--- server/routes/keyword.route.js
+++ server/routes/keyword.route.js
@@ -6,6 +6,8 @@ router.post('/create', keyword_controller.keyword_create);
 
 router.get('/getall', keyword_controller.keyword_getall);
 
+router.get('/getallkeys', keyword_controller.keyword_getallkeys);
+
 router.delete('/delete/:keyword', keyword_controller.keyword_delete);
 
 router.delete('/deleteByGroup/:group', keyword_controller.keyword_deleteByGroup);
