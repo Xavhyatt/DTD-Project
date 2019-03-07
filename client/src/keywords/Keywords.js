c2d0051f5575c427442cd2b0087a2180a2d18c65
ConradMichael <45197879+ConradMichael@users.noreply.github.com>
2019-03-07 14:36:39 +0000

    Add files via upload

--- client/src/keywords/Keywords.js
+++ client/src/keywords/Keywords.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './Keys.css';
 import {Button, ButtonGroup, Input} from "reactstrap";
 let toTest = [];
-let requestURL = "http://51.137.151.100:9123";
+//let requestURL = "http://51.137.151.100:9123";
 
-//let requestURL = "http://51.137.135.156:9123";
+let requestURL = "http://51.137.135.156:9123";
 
 function loadJSON(){
 	let request = new XMLHttpRequest();
