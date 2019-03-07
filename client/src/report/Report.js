8833c37cace98798fe427ed079ad28ba27baf582
ConradMichael <45197879+ConradMichael@users.noreply.github.com>
2019-03-07 14:36:17 +0000

    Add files via upload

--- client/src/report/Report.js
+++ client/src/report/Report.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { VictoryBar, VictoryChart, VictoryStack } from "victory";
+import {VictoryBar, VictoryChart, VictoryStack, VictoryAxis} from "victory";
 import ReportTableBody from "./reporttablebody.js";
 
 const wordys = require("../results/result.json");
@@ -16,59 +16,53 @@ const wordys = require("../results/result.json");
 
 class Report extends Component {
 
-	constructor() {
-		super();
-
-		this.state = {
-			filteredWords: wordys.exactMatches
-		}
-
-		this.filterGroup = () => {
-			this.setState({
-				filteredWords: wordys.exactMatches.filter((word) =>
-					word.Word.toLowerCase().includes(document.getElementById("wordSearch").value.toLowerCase()))
-			});
-		}
-	}
-
-
-
-
-	render() {
-		return (
+	render(){
+		return(
 			<div id="report">
-				<input className="form-control mr-sm-2" id="wordSearch" type="search" placeholder="Search" onChange={this.filterGroup}></input>
 				<h1> {name} </h1>
 				<h2> Threat Level: {threatLevel} </h2>
 				<h3> Threat Word Count: {numberOfThreatWordsFound}</h3>
 				<h3> Total Word Count: {wordcount}</h3>
 
-				<div>
-					<table className="table">
-						<thead>
+		
+				 <div>
+			 		<table className ="table">
+			 			<thead>
 							<tr>
-								<th>WORD</th>
-								<th>Frequency</th>
-							</tr>
-						</thead>
-						<ReportTableBody data={this.state.filteredWords} />
-					</table>
-				</div>
-
-
-
-				<VictoryChart
-					padding={{ top: 30, bottom: 70, left: 90, right: 20 }}>
+			 					<th>WORD</th>
+			 					<th>Frequency</th>
+			 				</tr>
+			 			</thead>
+			 			<ReportTableBody data ={wordys.exactMatches}/>
+			 		</table>
+				 </div>
+			 	<VictoryChart 
+			 	  padding={{ top: 30, bottom: 70, left: 90, right: 20 }}>
 					<VictoryStack
-						horizontal={true}>
-						<VictoryBar
-							data={this.state.filteredWords}
-							x="Word"
-							y="Frequency" />
-					</VictoryStack>
-				</VictoryChart>
+			 		horizontal ={true}>
+			 		<VictoryBar
+			 			style={{data: {fill: "#fd6b52"}}}
+			 			data={wordys.exactMatches}
+			 			x="Word"
+			 			y="Frequency"/> 
+			 		</VictoryStack>
+			 		<VictoryAxis
+			 			  style={{
+						    axis: {stroke: "#fd6b52" },
+						    ticks: {stroke: "#fd6b52", size: 5},
+						    tickLabels: {fontSize: 15, padding: 5, fill:"#fd6b52"}
+						  }} />
+    		<VictoryAxis dependentAxis
+    			  style={{
+						    axis: {stroke: "#fd6b52" },
+						    ticks: {stroke: "#fd6b52", size: 5},
+						    tickLabels: {fontSize: 10, padding: 2, fill: "#fd6b52"}
+						  }}
+
+    			/>
+			 	</VictoryChart> 
 			</div>
-		);
+			);
 	}
 }
 
--- client/src/upload/Uploads.jsx
+++ client/src/upload/Uploads.jsx
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import "./upload.css";
 
 class App extends Component {
   render() {
     return (
       <div className="Upload">
-          <form action="/" encType="multipart/form-data" method="post">
-            <input id="fileform" required="required" type="file" name="upload" accept=".pdf" multiple/>
-            <input type="submit" value="Upload"/>
-          </form>
+        <form action="/" encType="multipart/form-data" method="post">
+          <input
+            id="fileform"
+            required="required"
+            type="file"
+            name="upload"
+            accept=".pdf"
+            multiple
+          />
+          <Button type="submit" value="Upload">
+            Upload
+          </Button>
+        </form>
       </div>
     );
   }
